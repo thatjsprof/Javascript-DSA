1ffe0c8fa8e9437c8fea86ac895a0297b8600be3
thatjsprof <davidrums65@gmail.com>
2022-04-08 20:10:11 +0100

    correction of getindexnextdortedelement

--- LinkedList/sorted_linkedList_approach_1.js
+++ LinkedList/sorted_linkedList_approach_1.js
@@ -1,5 +1,8 @@
 const { LinkedList } = require("./single_approach_1");
 
+const Compare = {
+    LESS_THAN: ''
+}
 class SortedLinkedList extends LinkedList {
   constructor(equalFn) {
     super(equalFn);
@@ -21,7 +24,7 @@ class SortedLinkedList extends LinkedList {
     for (; i < this.count && current; i++) {
       const compare = this.equalFn(element, current.element);
 
-      if (compare) {
+      if (compare === Compare.LESS_THAN) {
         return i;
       }
       current = current.next;
